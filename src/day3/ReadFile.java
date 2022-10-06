d56b452cae452a4dba8d776966b758b297df6e10
cvivis <hsy0754@naver.com>
2022-10-06 15:16:40 +0900

    현재 디렉토리파일 목록들 출력

--- src/day3/ReadFile.java
+++ src/day3/ReadFile.java
@@ -1,6 +1,7 @@
 package day3;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -13,9 +14,10 @@ public class ReadFile {
         Scanner sc = new Scanner(System.in);
         System.out.println("몇 글자 읽으실래요?: ");
         int wordNum = sc.nextInt();
-        System.out.println(readFile.readNByte(wordNum,"a_file.txt"));
+        System.out.println(readFile.readNByte(wordNum, "a_file.txt"));
         System.out.println(readFile.readOneByte("a_file.txt"));
         System.out.println(readFile.readTwoByte("a_file.txt"));
+        readFile.fileList();
     }
 
     public String readNByte(int wordNum, String filename) throws IOException {
@@ -24,19 +26,21 @@ public class ReadFile {
                 16 * 1024
         );
         String str = "";
-        for(int i = 0; i<wordNum;i++){
-            str = str + (char)br.read();
+        for (int i = 0; i < wordNum; i++) {
+            str = str + (char) br.read();
         }
 
         return str;
     }
+
     public char readOneByte(String filename) throws IOException {
         BufferedReader br = new BufferedReader(
                 new FileReader(filename),
                 16 * 1024
         );
-        return (char)br.read();
+        return (char) br.read();
     }
+
     public String readTwoByte(String filename) throws IOException {
         BufferedReader br = new BufferedReader(
                 new FileReader(filename),
@@ -44,10 +48,36 @@ public class ReadFile {
         );
 
         String str = "";
-        for(int i = 0; i<2;i++){
-            str = str + (char)br.read();
+        for (int i = 0; i < 2; i++) {
+            str = str + (char) br.read();
         }
         return str;
     }
 
+    public void fileList() {
+        File dir = new File("./");
+        File[] files = dir.listFiles();
+        for (File file : files) {
+            System.out.println(file);
+        }
+
+    }
+
 }
+
+/*
+out put:
+몇 글자 읽으실래요?:
+3
+hel
+h
+he
+-------
+./out
+./java_exercise1005.iml
+./README.md
+./.gitignore
+./.git
+./a_file.txt
+./.idea
+./src   */
