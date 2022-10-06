35ee7a97229a4d1d5b7fa368f2ad4c7ec3ec84e7
cvivis <hsy0754@naver.com>
2022-10-06 13:18:43 +0900

    isAlphabet으로 알파벳 감별

--- src/day3/CountEnMain.java
+++ src/day3/CountEnMain.java
@@ -7,7 +7,7 @@ public class CountEnMain {
         String str = "https://github.com/cvivis/git_exercices".toLowerCase();
         HashMap<Character, Integer> map = new HashMap<>();
         for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) >= 'a' && str.charAt(i) <= 'z') {
+            if (isAlphabet(str.charAt(i))) {
                 if(map.get(str.charAt(i))!=null){ // 처음엔 아무것도 없어서 Null 가리킴
                     map.put(str.charAt(i), map.get(str.charAt(i)) + 1);
                 }else{
@@ -16,5 +16,12 @@ public class CountEnMain {
             }
         }
         System.out.println(map);
+    }
+    public static boolean isAlphabet(char c){
+        if(c >= 'a' && c <= 'z'){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
