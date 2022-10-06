7346b537bef6a66b43a41341d4226d715df33cd5
cvivis <hsy0754@naver.com>
2022-10-06 12:06:49 +0900

    알파벳 갯수세기

---
+++ src/day3/CountEnMain.java
@@ -0,0 +1,20 @@
+package day3;
+
+import java.util.HashMap;
+
+public class CountEnMain {
+    public static void main(String[] args) {
+        String str = "https://github.com/cvivis/git_exercices".toLowerCase();
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) >= 'a' && str.charAt(i) <= 'z') {
+                if(map.get(str.charAt(i))!=null){ // 처음엔 아무것도 없어서 Null 가리킴
+                    map.put(str.charAt(i), map.get(str.charAt(i)) + 1);
+                }else{
+                    map.put(str.charAt(i), 1);
+                }
+            }
+        }
+        System.out.println(map);
+    }
+}
