9a343a1ef6d7bc0b657c4aa6bcec502a6129ce8f
cvivis <hsy0754@naver.com>
2022-10-06 14:45:50 +0900

    n입력받아 n글자 출력하기

--- src/day3/ReadFile.java
+++ src/day3/ReadFile.java
@@ -5,19 +5,28 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class ReadFile {
     public static void main(String[] args) throws IOException {
         ReadFile readFile = new ReadFile();
-        System.out.println(readFile.readOneByte("./a_file.txt"));
+        Scanner sc = new Scanner(System.in);
+        System.out.println("몇 글자 읽으실래요?: ");
+        int wordNum = sc.nextInt();
+        System.out.println(readFile.readOneByte(wordNum,"a_file.txt"));
     }
 
-    public char readOneByte(String filename) throws IOException {
+    public String readOneByte(int wordNum, String filename) throws IOException {
         BufferedReader br = new BufferedReader(
                 new FileReader(filename),
                 16 * 1024
         );
-        return (char)br.read();
+        String str = "";
+        for(int i = 0; i<wordNum;i++){
+            str = str + (char)br.read();
+        }
+
+        return str;
     }
 
 }
