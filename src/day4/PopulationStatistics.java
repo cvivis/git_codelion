319a4689af2c2dc359bfe8437bb4178738c818b1
cvivis <hsy0754@naver.com>
2022-10-07 14:51:30 +0900

    코드 한국어 변환 메소드로 바꿈

--- src/day4/PopulationStatistics.java
+++ src/day4/PopulationStatistics.java
@@ -62,7 +62,7 @@ public class PopulationStatistics {
         }
     }
 
-    public String fromToString(PopulationMove populationMove){
+    public String fromToString(PopulationMove populationMove) {
         return populationMove.getFromSido() + " , " + populationMove.getToSido();
     }
 
@@ -80,7 +80,7 @@ public class PopulationStatistics {
 
         try {
             BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
-            for(String str : strs){
+            for (String str : strs) {
                 bufferedWriter.write(str);
             }
             System.out.println("Write 완료");
@@ -101,20 +101,52 @@ public class PopulationStatistics {
         }
         return moveCntmap;
     }
+
+    public List<String> changeKr(List<PopulationMove> pml) {
+        List<String> pmlKr = new ArrayList<>();
+        HashMap<Integer, String> adm_code = new HashMap<>();
+        adm_code.put(11, "서울");
+        adm_code.put(26, "부산");
+        adm_code.put(27, "대구");
+        adm_code.put(28, "인천");
+        adm_code.put(29, "광주");
+        adm_code.put(30, "대전");
+        adm_code.put(31, "울산");
+//        adm_code.put(29,"세종");
+        adm_code.put(41, "경기도");
+        adm_code.put(42, "강원도");
+        adm_code.put(43, "충청북도");
+        adm_code.put(44, "충청남도");
+        adm_code.put(45, "전라북도");
+        adm_code.put(46, "전라남도");
+        adm_code.put(47, "경상북도");
+        adm_code.put(48, "경상남도");
+        adm_code.put(50, "제주도");
+
+        String fromSido = "";
+        String toSido = "";
+
+        for (PopulationMove pm : pml) {
+            fromSido = adm_code.get(pm.getFromSido());
+            toSido = adm_code.get(pm.getToSido());
+            String s = fromSido + "," + toSido;
+            pmlKr.add(s);
+        }
+        return pmlKr;
+    }
+
     public static void main(String[] args) throws IOException {
         String address = "/Users/admin/Documents/data/2021_인구관련연간자료_20220927_66125.csv";
         PopulationStatistics populationStatistics = new PopulationStatistics();
         List<PopulationMove> pml = populationStatistics.parse(address);
         List<String> pmlWrite = new ArrayList<>(); // for Write
-        populationStatistics.parse(address);
-        for(PopulationMove pm : pml){
-//            pmlWrite.add(populationStatistics.fromToString(pm)) ;
-////            System.out.println(populationStatistics.fromToString(pm));
-//            pm.toString();
+        List<String> pmlKr = populationStatistics.changeKr(pml);
+        for (String pmkr : pmlKr) {
+            System.out.println(pmkr);
         }
-        Map<String,Integer> map = populationStatistics.getMoveCntMap(pml);
-        for(String key : map.keySet()){
-            String s = String.format("값:%s, 갯수:%d\n",key,map.get(key));
+        Map<String, Integer> map = populationStatistics.getMoveCntMap(pml);
+        for (String key : map.keySet()) {
+            String s = String.format("값:%s, 갯수:%d\n", key, map.get(key));
             pmlWrite.add(s);
 //            System.out.printf("값:%s, 갯수:%d\n",key,map.get(key));
         }
@@ -123,6 +155,6 @@ public class PopulationStatistics {
 //        populationStatistics.readLine(address);
 //        populationStatistics.readLine2(address);
 //            populationStatistics.createAfile("frome_to.txt");
-        populationStatistics.write(pmlWrite,"SidoCount.txt");
+        populationStatistics.write(pmlWrite, "SidoCount.txt");
     }
 }
