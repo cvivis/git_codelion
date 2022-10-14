514a076049093d4158d3ea7019263dd2c1747c7e
cvivis <hsy0754@naver.com>
2022-10-14 16:13:12 +0900

    코드업 1001~ 추가

---
+++ src/codeUp/p1001.java
@@ -0,0 +1,62 @@
+package codeUp;
+
+public class p1001 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
+//public class Code1002 {
+//    public static void main(String[] args) {
+//        System.out.println("Hello World");
+//}
+
+//public class Code1003 {
+//    public static void main(String[] args) {
+//        System.out.println("Hello");
+//        System.out.println("World");
+//    }
+//}
+
+//public class Code1004 {
+//    public static void main(String[] args) {
+//        System.out.println("\'Hello\'");
+//    }
+//}
+
+//public class Code1005 {
+//    public static void main(String[] args) {
+//        System.out.println("\"Hello World\"");
+//    }
+//}
+
+//public class Code1006 {
+//    public static void main(String[] args) {
+//        System.out.println("\"!@#$%^&*()\"");
+//    }
+//}
+
+//public class Code1007 {
+//    public static void main(String[] args) {
+//        System.out.println("\"C:\\Download\\hello.cpp\"");
+//    }
+//}
+
+//public class Code1008 {
+//    public static void main(String[] args) {
+//        System.out.printf("\u250C\u252C\u2510\n");
+//        System.out.printf("\u251C\u253C\u2524\n");
+//        System.out.printf("\u2514\u2534\u2518");
+//    }
+//}
+
+//import java.util.Scanner;
+//
+//public class Code1010 {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int num = sc.nextInt();
+//        System.out.println(num);
+//    }
+//}
+
