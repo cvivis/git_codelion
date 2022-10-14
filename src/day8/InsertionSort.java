2f1ebb6aa9184891a645fe906178f4a96adeb62a
cvivis <hsy0754@naver.com>
2022-10-14 10:34:50 +0900

    삽입정렬 이중 for문 버전

--- src/day8/InsertionSort.java
+++ src/day8/InsertionSort.java
@@ -5,17 +5,25 @@ import java.util.Arrays;
 public class InsertionSort {
 
     public int[] sort(int[] arr){
-        int temp = 1;
-        int target = 0;
-        for(int i = 1 ; i<arr.length;i++ ){
-            target = arr[i];
-            int j = i - 1;
-            while(j>=0 && target<arr[j]){
-                arr[j+1] = arr[j] ;
-                j--;
-            }
-            arr[j+1] = target;
+//        int target = 0;
+//        for(int i = 1 ; i<arr.length;i++ ){
+//            target = arr[i];
+//            int j = i - 1;
+//            while(j>=0 && target<arr[j]){
+//                arr[j+1] = arr[j] ;
+//                j--;
+//            }
+//            arr[j+1] = target;
+//        }
 
+        for(int i = 1 ; i<arr.length;i++ ){
+            for(int j = i - 1; j>=0;j--){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
         }
 
         return arr;
