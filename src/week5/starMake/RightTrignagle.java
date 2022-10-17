4fd6f1ce02cb0beda017060073ba043e163c4b05
cvivis <hsy0754@naver.com>
2022-10-17 09:39:27 +0900

    마름모

--- src/week5/starMake/RightTrignagle.java
+++ src/week5/starMake/RightTrignagle.java
@@ -11,12 +11,24 @@ public class RightTrignagle {
             System.out.println("\n");
         }
     }
+    public void square(int num){
+        for (int i = 0; i <= num; i++) {
+            for(int k = 0;k<num-i;k++){
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2*i-1; j++) {
+                System.out.print("*");
+            }
+            System.out.println("\n");
+        }
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         RightTrignagle rightTrignagle = new RightTrignagle();
         int num = sc.nextInt();
-        rightTrignagle.right(num);
+//        rightTrignagle.right(num);
+        rightTrignagle.square(num);
 
     }
 }
