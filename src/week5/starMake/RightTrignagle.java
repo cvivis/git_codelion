daeec45a4fdb70d7d92088dc232dff57c449a98e
cvivis <hsy0754@naver.com>
2022-10-17 09:19:46 +0900

    함수 분리

--- src/week5/starMake/RightTrignagle.java
+++ src/week5/starMake/RightTrignagle.java
@@ -3,15 +3,20 @@ package week5.starMake;
 import java.util.Scanner;
 
 public class RightTrignagle {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
+    public void right(int num){
         for(int i = 0; i<=num;i++){
             for(int j = 0; j<i;j++){
                 System.out.print("*");
             }
             System.out.println("\n");
         }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        RightTrignagle rightTrignagle = new RightTrignagle();
+        int num = sc.nextInt();
+        rightTrignagle.right(num);
 
     }
 }
