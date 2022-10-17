2468c5a94772051b9e1e024a1201031b72af8447
cvivis <hsy0754@naver.com>
2022-10-17 10:19:29 +0900

    마름모 출력

--- src/week5/starMake/RightTrignagle.java
+++ src/week5/starMake/RightTrignagle.java
@@ -20,7 +20,7 @@ public class RightTrignagle {
             System.out.println("\n");
         }
     }
-    public void square(int num){
+    public void pyramid(int num){
         for (int i = 0; i <= num; i++) {
             for(int k = 0;k<num-i;k++){
                 System.out.print(" ");
@@ -31,13 +31,37 @@ public class RightTrignagle {
             System.out.println("\n");
         }
     }
+    public void square(int num){
+        //마름모 위에(가운데 포함)
+        for (int i = 0; i <= num; i++) {
+            for(int k = 0;k<num-i;k++){
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2*i-1; j++) {
+                System.out.print(letter);
+            }
+            System.out.println("\n");
+        }
+        //마름모 아래
+        for (int l = num-1; l >=0; l--) {
+            for (int o = 0; o < num - l; o++) {
+                System.out.print(" ");
+            }
+            for (int m = 1; m <= 2 * l - 1; m++) {
+                System.out.print(letter);
+            }
+
+
+            System.out.println("\n");
+        }
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         RightTrignagle rightTrignagle = new RightTrignagle("#");
         int num = sc.nextInt();
 //        rightTrignagle.right(num);
+//        rightTrignagle.pyramid(num);
         rightTrignagle.square(num);
-
     }
 }
