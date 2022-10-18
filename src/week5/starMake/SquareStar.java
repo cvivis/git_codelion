1b3a2a25ed556cd55c76c3449681055c53109800
cvivis <hsy0754@naver.com>
2022-10-18 09:14:23 +0900

    직사각형 별찍기

--- src/week5/starMake/SquareStar.java
+++ src/week5/starMake/SquareStar.java
@@ -11,10 +11,19 @@ public class SquareStar {
         }
     }
 
+    public void RactangleMatrix(int n ,int m){
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println("\n");
+        }
+    }
+
     public static void main(String[] args) {
 
         SquareStar squareStar = new SquareStar();
         squareStar.printSquareMatrix(4);
-
+        squareStar.RactangleMatrix(3,4);
     }
 }
